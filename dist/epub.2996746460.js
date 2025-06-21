ff969d4a4b308a6515b68190838bee83385d9b51
ArnoCB <a.bouwes@upcmail.nl>
2025-06-21 13:17:34 +0200

    chore(build): speedpub install, by prebuilding and offering a dist

---
+++ dist/epub.2996746460.js
@@ -0,0 +1,22 @@
+"use strict";
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["ePub"] = factory();
+	else
+		root["ePub"] = factory();
+})(self, () => {
+return (self["webpackChunkePub"] = self["webpackChunkePub"] || []).push([["epub.2996746460"],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["commons"], () => (__webpack_exec__("./test/epub.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ __webpack_exports__ = __webpack_exports__["default"];
+/******/ return __webpack_exports__;
+/******/ }
+]);
+});
+//# sourceMappingURL=epub.2996746460.js.map
---
+++ dist/locations.4185668146.js
@@ -0,0 +1,22 @@
+"use strict";
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["ePub"] = factory();
+	else
+		root["ePub"] = factory();
+})(self, () => {
+return (self["webpackChunkePub"] = self["webpackChunkePub"] || []).push([["locations.4185668146"],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["commons"], () => (__webpack_exec__("./test/locations.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ __webpack_exports__ = __webpack_exports__["default"];
+/******/ return __webpack_exports__;
+/******/ }
+]);
+});
+//# sourceMappingURL=locations.4185668146.js.map
